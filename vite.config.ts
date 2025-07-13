b6f02645e8a575fd802d8dfc1d04ede97c560a71
WishvanathOfficial <wishvanath.sah.official@gmail.com>
2025-07-13 11:02:57 +0530

    Updated script for CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sequalize-school/',
+  server: {
+    port: 3000,
+    host: 'localhost',
+    open: true,
+  },  
   plugins: [react()],
 })
